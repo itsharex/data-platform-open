ded1957c5da8d27d1d2c61dda1e65f5cff904f5e
qwding <761945125@qq.com>
2025-08-25 19:36:04 +0800

    优化

--- data-platform-open-common/src/main/java/cn/dataplatform/open/common/util/VersionUtils.java
+++ data-platform-open-common/src/main/java/cn/dataplatform/open/common/util/VersionUtils.java
@@ -33,7 +33,9 @@ public class VersionUtils {
         if (minorVersion) {
             return versionSplit[0] + "." + (Integer.parseInt(versionSplit[1]) + 1);
         }
-        return (Integer.parseInt(versionSplit[0]) + 1) + "." + versionSplit[1];
+        // return (Integer.parseInt(versionSplit[0]) + 1) + "." + versionSplit[1];
+        // 大版本升级后，小版本号归0
+        return (Integer.parseInt(versionSplit[0]) + 1) + ".0";
     }
 
     /**
